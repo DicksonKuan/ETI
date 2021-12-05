00ad8f7e364ad3c75d00bc30c864b60104d6981c
Dickson Kuan <s10192803@connect.np.edu.sg>
2021-12-05 21:08:43 +0800

    Updated readme instructions and a bit of design consideration

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Dickson, S10192803
 
 1. Design consideration
-
+- The database tables are not connected to each other so that each Microservice would have thier own database table. 
+- The 3 microservice are able to communicate with each other via API. This is to allow them to validate certain data
 
 
 2. Architecture diagram
@@ -11,11 +12,29 @@ Dickson, S10192803
 3. Instructions for setting up and running the microservice
 3.1 Setting up database
 - Download XAMPP
+- Update Portnumber to 3308 in XAMPP Config file
+* https://www.phpflow.com/php/how-to-change-xampp-apache-server-port/ - instructions
 - Go to PHPMyAdmin
+- Set password for all account
+* UserAccount > root > change password to 'password'
 - Create new database 
-- Insert SQL code 
+- Insert SQL code via 'rideshare.sql' OR 'SQL.txt'
+* Please do not insert all the SQL statement at once for SQL.txt
 - Click 'GO'
 
-Lib used
+3.2 Run API
+- Launch command prompt and run 
+* Trip folder, Main.go
+* Customer folder, Main.go
+* Driver folder, Main.go
+
+3.3 Test API
+- Run html files 
+* Front-end folder > Trip/passenger/ driver.html
+
+Lib and platform used
 1. https://github.com/gorilla/mux
 2. XAMPP
+3. https://github.com/gorilla/handlers
+4. Visual studio code
+5. https://github.com/go-sql-driver/mysql
---
+++ rideshare.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Dec 05, 2021 at 01:56 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rideshare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `ID` int(5) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `MobileNumber` varchar(8) NOT NULL,
+  `EmailAddress` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `EmailAddress`, `Password`) VALUES
+(1, 'Susan', 'Wiki', '91234568', 'Susan@np.com', 'password'),
+(2, 'Willy', 'Wiki', '91234569', 'Willy@np.com', 'password');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver`
+--
+
+CREATE TABLE `driver` (
+  `DriverID` varchar(255) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `MobileNo` varchar(8) NOT NULL,
+  `EmailAddress` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `CarLiscenseNo` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `driver`
+--
+
+INSERT INTO `driver` (`DriverID`, `FirstName`, `LastName`, `MobileNo`, `EmailAddress`, `Password`, `CarLiscenseNo`) VALUES
+('S1234567A', 'Ethan', 'Wiki', '91234567', 'Ethan@np.com', 'password', 'S123LO'),
+('S1234567B', 'Elvan', 'Wiki', '91234570', 'Elvan@np.com', 'password', 'S124LO'),
+('S1234567C', 'Azzi', 'Wiki', '91234570', 'Azzi@np.com', 'password', 'S125LO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip`
+--
+
+CREATE TABLE `trip` (
+  `TripID` int(11) NOT NULL,
+  `DriverID` varchar(255) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `PickUpLocation` varchar(255) NOT NULL,
+  `DropOffLocation` varchar(255) NOT NULL,
+  `PickUpTime` varchar(255) NOT NULL,
+  `DropOffTime` varchar(255) NOT NULL,
+  `Status` varchar(255) NOT NULL CHECK (`Status` in ('Pending','On The Way','In Transit','Completed','Failed'))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trip`
+--
+
+INSERT INTO `trip` (`TripID`, `DriverID`, `CustomerID`, `PickUpLocation`, `DropOffLocation`, `PickUpTime`, `DropOffTime`, `Status`) VALUES
+(1, 'S1234567A', 1, '123456', '123457', '12:30', '13:00', 'Completed'),
+(2, 'S1234567A', 2, '123456', '123457', '12:30', ' ', 'Pending');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `driver`
+--
+ALTER TABLE `driver`
+  ADD PRIMARY KEY (`DriverID`),
+  ADD UNIQUE KEY `DriverID` (`DriverID`);
+
+--
+-- Indexes for table `trip`
+--
+ALTER TABLE `trip`
+  ADD PRIMARY KEY (`TripID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `trip`
+--
+ALTER TABLE `trip`
+  MODIFY `TripID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
